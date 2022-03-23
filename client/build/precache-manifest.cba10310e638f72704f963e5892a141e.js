a0de78fdf38fdae83680ab54b9df7244bbd8cda7
Jason Wisniewski <jwizinpa95@gmail.com>
2022-03-23 17:59:03 -0600

    end of module

---
+++ client/build/precache-manifest.cba10310e638f72704f963e5892a141e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7911e818f336f49ab68c9ebce48e1ce1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caa30bc38d851e63dd5f",
+    "url": "/static/css/main.db415adc.chunk.css"
+  },
+  {
+    "revision": "130c744298700e5891bb",
+    "url": "/static/js/2.03f92a88.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.03f92a88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caa30bc38d851e63dd5f",
+    "url": "/static/js/main.6d98fd61.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
